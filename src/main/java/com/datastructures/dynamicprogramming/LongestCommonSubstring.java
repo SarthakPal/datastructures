aa7403e2c523c1875b88f35686f6a6733f15580b
SarthakPal <sarthak.pal.sharma@gmail.com>
2024-01-04 10:22:58 +0530

    added LCS and LPS problems

---
+++ src/main/java/com/datastructures/dynamicprogramming/LongestCommonSubstring.java
@@ -0,0 +1,58 @@
+package com.datastructures.dynamicprogramming;
+
+public class LongestCommonSubstring {
+
+    static int dp[][] = new int[1001][1001];
+
+    public static int longestCommonSubstring(String a, String b, int m , int n)
+    {
+        for(int i=0;i<=m;i++)
+        {
+            for(int j=0;j<=n;j++)
+            {
+                if(i==0)
+                {
+                    dp[i][j]=0;
+                }
+                else if(j==0)
+                {
+                    dp[i][j]=0;
+                }
+                else if(a.charAt(i-1)==b.charAt(j-1))
+                {
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return dp[m][n];
+    }
+    public static void main(String args[])
+    {
+        String a = "abcd";
+        String b = "abcdef";
+
+        int m = a.length();
+        int n = b.length();
+        int max = Integer.MIN_VALUE;
+
+        longestCommonSubstring(a, b, m, n);
+
+        for(int i=0;i<=m;i++)
+        {
+            for(int j=0;j<=n;j++)
+            {
+                if(dp[i][j]>=max)
+                {
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+
+        System.out.println(max);
+    }
+
+}
