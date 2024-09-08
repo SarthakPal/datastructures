843a75222782f1ae2a15bea8791b7803ff239f8f
SarthakPal <sarthak.pal.sharma@gmail.com>
2024-09-08 06:42:57 +0530

    added array problems

--- src/main/java/com/datastructures/arrays/ReversePairs.java
+++ src/main/java/com/datastructures/arrays/ReversePairs.java
@@ -5,71 +5,61 @@ import java.util.List;
 
 public class ReversePairs {
 
-    public static void merge(int nums[], int low, int mid, int high)
-    {
+    public static void merge(int nums[], int low, int mid, int high) {
         List<Integer> list = new ArrayList<>();
 
         int left = low;
-        int right = mid+1;
+        int right = mid + 1;
 
-        while(left<=mid && right<=high)
-        {
-            if(nums[left]<=nums[right])
-            {
+        while (left <= mid && right <= high) {
+            if (nums[left] <= nums[right]) {
                 list.add(nums[left]);
                 left++;
-            }
-            else
-            {
+            } else {
                 list.add(nums[right]);
                 right++;
             }
         }
 
-        while(left<=mid)
-        {
+        while (left <= mid) {
             list.add(nums[left]);
             left++;
         }
 
-        while(right<=high)
-        {
+        while (right <= high) {
             list.add(nums[right]);
             right++;
         }
 
-        for(int i=low;i<=high;i++)
-        {
-            nums[i] = list.get(i-low);
+        for (int i = low; i <= high; i++) {
+            nums[i] = list.get(i - low);
         }
     }
 
-    public static int countPairs(int nums[], int low, int mid, int high)
-    {
+    public static int countPairs(int nums[], int low, int mid, int high) {
         int count = 0;
-        int right = mid+1;
-        for(int i=low;i<=mid;i++)
-        {
-            while(right<=high && (long) nums[i] > 2L * nums[right])
-            {
+        int left = low;
+        int right = mid + 1;
+        while (left <= mid && right <= high) {
+            if ((long) nums[left] > 2L * nums[right]) {
+                count += (mid - left + 1); // All elements from left to mid form a pair with nums[right]
                 right++;
+            } else {
+                left++;
             }
-            count+=(right - (mid+1));
         }
         return count;
     }
 
-    public static int mergeSort(int nums[], int low, int high)
-    {
-        int count=0;
-        if(low>=high)
-        {
+    public static int mergeSort(int nums[], int low, int high) {
+        int count = 0;
+        if (low >= high) {
             return count;
         }
-        int mid = (low+high)/2;
-        count+=mergeSort(nums, low, mid);
-        count+=mergeSort(nums, mid+1, high);
-        count+=countPairs(nums, low, mid, high);
+        int mid = (low + high) / 2;
+        count += mergeSort(nums, low, mid);
+        count += mergeSort(nums, mid + 1, high);
+        count += countPairs(nums, low, mid, high);
         merge(nums, low, mid, high);
         return count;
     }
@@ -77,7 +67,7 @@ public class ReversePairs {
     public static void main(String[] args) {
         int[] nums = {2147483647, 2147483647, 2147483647, 2147483647, 2147483647, 2147483647};
         int n = 5;
-        int cnt = mergeSort(nums, 0, n-1);
+        int cnt = mergeSort(nums, 0, n - 1);
         System.out.println("The number of reverse pair is: " + cnt);
     }
 
